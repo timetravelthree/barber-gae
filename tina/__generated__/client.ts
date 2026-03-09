d5a2dd86a8b11f23bd933a2abd2c0eaa3fcb3d57
Alessio01 <user@users-MacBook-Air.local>
2026-03-09 23:39:35 +0100

    init tinacms with generated schema

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,2 +1,5 @@
-export default ()=>({})
-export const client = ()=>({})
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: '/Users/user/Workspace/web-dev/barber/tina/__generated__/.cache/1773095877034', url: 'https://content.tinajs.io/2.2/content/160598d4-00c6-4b53-84d8-c1d2db306fc4/github/main', token: 'ccd2f2369528bdc719c41146169cc63945704abe', queries,  });
+export default client;
+  
