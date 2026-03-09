a7c0189f66c2b89b9cb4f36cc9e829fbbc4f43fe
Alessio01 <user@users-MacBook-Air.local>
2026-03-10 00:25:28 +0100

    add pre-built admin and vercel config

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/user/Workspace/web-dev/barber/tina/__generated__/.cache/1773098546663', url: 'https://content.tinajs.io/2.2/content/160598d4-00c6-4b53-84d8-c1d2db306fc4/github/main', token: 'ccd2f2369528bdc719c41146169cc63945704abe', queries,  });
+export const client = createClient({ cacheDir: '/Users/user/Workspace/web-dev/barber/tina/__generated__/.cache/1773098606302', url: 'https://content.tinajs.io/2.2/content/160598d4-00c6-4b53-84d8-c1d2db306fc4/github/main', token: 'ccd2f2369528bdc719c41146169cc63945704abe', queries,  });
 export default client;
   
